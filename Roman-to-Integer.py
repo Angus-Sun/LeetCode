1eb7cfa2d9095834a86adaa8eda61116c96285b5
Angus Sun <135078117+Angus-Sun@users.noreply.github.com>
2025-11-14 01:25:48 -0500

    [13] [Time Beats: 63.82%] [Memory Beats: 89.88%] - LeetPush

---
+++ Roman-to-Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        sum = 0
+        for i in range(len(s)-1):
+            if (numerals[s[i]] < numerals[s[i+1]]):
+                sum -= numerals[s[i]]
+            else:
+                sum += numerals[s[i]]
+        sum += numerals[s[len(s)-1]]
+        return sum
+                
