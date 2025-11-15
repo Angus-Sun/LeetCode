5a4ee5a0cb02cc488d7b3a5762312fe7090511f3
Angus Sun <135078117+Angus-Sun@users.noreply.github.com>
2025-11-14 23:12:22 -0500

    [7] [Time Beats: 81.27%] [Memory Beats: 57.33%] - LeetPush

---
+++ Reverse-Integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        integer = str(x)
+        if integer[0] == '-':
+            integer = integer[1:]
+            integer += '-'
+        result = int(integer[::-1])
+        if result < -2**31 or result > 2**31-1:
+            return 0
+        return result
