733ec8699e08da0e1b11e0f6e6a73a86de88e040
Angus Sun <a86sun@uwaterloo.ca>
2026-01-08 20:59:51 -0500

    Fix indentation in Longest-Consecutive-Sequence.py

--- Longest-Consecutive-Sequence.py
+++ Longest-Consecutive-Sequence.py
@@ -1,13 +1,12 @@
-1class Solution:
-2    def longestConsecutive(self, nums: List[int]) -> int:
-3        longestSequence = 0
-4        numSet = set(nums)
-5
-6        for num in numSet:
-7            if (num-1) not in numSet:
-8                length = 1
-9                while (num + length) in numSet:
-10                    length += 1
-11                longestSequence = max(length, longestSequence)
-12        return longestSequence
-13
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longestSequence = 0
+        numSet = set(nums)
+
+        for num in numSet:
+            if (num-1) not in numSet:
+                length = 1
+                while (num + length) in numSet:
+                    length += 1
+                longestSequence = max(length, longestSequence)
+        return longestSequence
