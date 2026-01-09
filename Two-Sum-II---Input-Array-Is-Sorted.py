cb0ede1eba02b45fc6896cdc7cf615e6b5ef45d5
Angus Sun <a86sun@uwaterloo.ca>
2026-01-08 20:59:04 -0500

    Fix formatting issues in Two-Sum-II solution

--- Two-Sum-II---Input-Array-Is-Sorted.py
+++ Two-Sum-II---Input-Array-Is-Sorted.py
@@ -1,9 +1,9 @@
-1class Solution:
-2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-3        i,j = 0, len(numbers)-1
-4        while (numbers[i]+numbers[j] != target):
-5            if numbers[i]+numbers[j] > target:
-6                j-=1
-7            else:
-8                i+=1
-9        return [i+1, j+1]
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i,j = 0, len(numbers)-1
+        while (numbers[i]+numbers[j] != target):
+            if numbers[i]+numbers[j] > target:
+                j-=1
+            else:
+                i+=1
+        return [i+1, j+1]
