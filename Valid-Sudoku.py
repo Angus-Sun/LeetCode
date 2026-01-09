3ca0f8b9f374bd15e21f0cf039f695707f87fd18
Angus Sun <a86sun@uwaterloo.ca>
2026-01-08 21:00:46 -0500

    Refactor Valid-Sudoku.py for consistency

--- Valid-Sudoku.py
+++ Valid-Sudoku.py
@@ -1,22 +1,16 @@
-1class Solution:
-2    def isValidSudoku(self, board: List[List[str]]) -> bool:
-3        rows = defaultdict(set)
-4        cols = defaultdict(set)
-5        squares = defaultdict(set)
-6
-7        for i in range(9):
-8            for j in range(9):
-9                if board[i][j] == ".":
-10                    continue
-11                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[i//3, j//3]:
-12                    return False
-13                rows[i].add(board[i][j])
-14                cols[j].add(board[i][j])
-15                squares[i//3, j//3].add(board[i][j])
-16        return True
-17                 
-18
-19
-20            
-21                
-22
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[i//3, j//3]:
+                    return False
+                rows[i].add(board[i][j])
+                cols[j].add(board[i][j])
+                squares[i//3, j//3].add(board[i][j])
+        return True
