7913b4da34b91e9eaca4db5ae8f60abe8014426e
Angus Sun <a86sun@uwaterloo.ca>
2026-01-12 23:59:53 -0500

    [704] [Time Beats: 100.00%] [Memory Beats: 9.26%] - LeetPush

---
+++ Binary-Search.py
@@ -0,0 +1,13 @@
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        low = 0
+4        high = len(nums)-1
+5        while (low <= high):
+6            mid = low + (high - low) // 2
+7            if nums[mid] == target:
+8                return mid
+9            elif nums[mid] > target:
+10                high = mid-1
+11            else:
+12                low = mid + 1
+13        return -1
