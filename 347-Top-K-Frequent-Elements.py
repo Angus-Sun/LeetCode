473b20b355234625b5ee758b91a6d57b20296b51
Angus Sun <a86sun@uwaterloo.ca>
2026-08-04 15:00:03 -0700

    [347] [Time Beats: 25.07%] [Memory Beats: 26.56%] - LeetPush

--- 347-Top-K-Frequent-Elements.py
+++ 347-Top-K-Frequent-Elements.py
@@ -9,7 +9,7 @@ class Solution:
             freq[num_map[num]].append(num)
         
         res = []
-        for i in range(len(nums), 0, -1):
+        for i in range(len(freq)-1, 0, -1):
             for num in freq[i]:
                 res.append(num)
                 if len(res) == k:
