cc97b46d4f41021920900466ac0a3783811432fe
Angus Sun <a86sun@uwaterloo.ca>
2026-08-09 16:52:17 -0700

    [238] [Time Beats: 62.28%] [Memory Beats: 94.09%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -1,12 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1]
-        temp = 1
-        for i in range(0, len(nums)-1):
-            temp *= nums[i]
-            res.append(temp)
-        temp = 1
+        product = 1
+        for i in range(len(nums)-1):
+            product *= nums[i]
+            res.append(product)
+        # 1,2,3,4
+        # 1, 1, 2, 6
+        # 24  12  4 1
+        product = 1
         for i in range(len(nums)-2, -1, -1):
-            temp *= nums[i+1]
-            res[i] *= temp
+            product *= nums[i+1]
+            res[i] *= product
         return res
