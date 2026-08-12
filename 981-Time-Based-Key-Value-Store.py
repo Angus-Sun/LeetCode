e01e5929c481266b57c20886961943c9687cda25
Angus Sun <a86sun@uwaterloo.ca>
2026-08-11 17:23:07 -0700

    [981] [Time Beats: 44.42%] [Memory Beats: 68.79%] - LeetPush

--- 981-Time-Based-Key-Value-Store.py
+++ 981-Time-Based-Key-Value-Store.py
@@ -1,22 +1,25 @@
 class TimeMap:
-
+#hashmap of key : list of (timestamp, val)
     def __init__(self):
-        self.time_store = defaultdict(list) # key : list of (timestamp, value)s
+        self.key_map = defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.time_store[key].append((timestamp, value))
+        self.key_map[key].append((timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        res, values = "", self.time_store[key]
+        values = self.key_map[key]
+        if not values:
+            return ""
         l, r = 0, len(values)-1
+        largest = ""
         while l <= r:
             m = l + (r-l) // 2
             if values[m][0] <= timestamp:
-                res = values[m][1]
+                largest = values[m][1]
                 l = m+1
             else:
                 r = m-1
-        return res
+        return largest 
 
 
 # Your TimeMap object will be instantiated and called as such:
