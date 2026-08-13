9068521cf7c509d3e2f402b9f192d414ba2681aa
Angus Sun <a86sun@uwaterloo.ca>
2026-08-12 21:42:57 -0700

    [76] [Time Beats: 46.29%] [Memory Beats: 73.43%] - LeetPush

--- 76-Minimum-Window-Substring.py
+++ 76-Minimum-Window-Substring.py
@@ -2,29 +2,25 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        
-        window, t_map = {}, {}
-
+        s_map, t_map = {}, {}
+        res, resLen = [-1,-1], float("infinity")
         for i in range(len(t)):
             t_map[t[i]] = 1 + t_map.get(t[i], 0)
-        
         have, need = 0, len(t_map)
-        res, resLen = [-1, -1], float("infinity")
         l = 0
         for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
-            if c in t_map and window[c] == t_map[c]:
+            if s[r] in t_map and s_map.get(s[r], 0) + 1 == t_map[s[r]]:
                 have += 1
-            
+            s_map[s[r]] = 1 + s_map.get(s[r], 0)
             while have == need:
-                if r-l+1 < resLen:
-                    res, resLen = [l, r], r-l+1
-                c = s[l]
-                window[c] -= 1
-                if c in t_map and window[c] < t_map[c]:
+                if r - l + 1 < resLen:
+                    res = [l, r]
+                    resLen = r-l+1
+                if s[l] in t_map and s_map.get(s[l], 0) == t_map[s[l]]:
                     have -= 1
+                s_map[s[l]] -= 1
                 l += 1
-        l, r = res
-        return s[l:r+1] if resLen < float("infinity") else ""
-                
+        l,r = res
+        return s[l:r+1] if resLen != float("infinity") else ""
+            
+
