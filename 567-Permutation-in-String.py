e1e35779a18b901f4598bcb09f4809798cbfe331
Angus Sun <a86sun@uwaterloo.ca>
2026-08-12 18:39:22 -0700

    [567] [Time Beats: 96.35%] [Memory Beats: 18.19%] - LeetPush

--- 567-Permutation-in-String.py
+++ 567-Permutation-in-String.py
@@ -1,19 +1,37 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s2) < len(s1):
             return False
-        s1map, s2map = {}, {}
-        for c in s1:
-            s1map[c] = 1+ s1map.get(c, 0)
-        l, r = 0, len(s1)-1
-        for i in range(r):
-            s2map[s2[i]] = 1 + s2map.get(s2[i], 0)
-        for i in range(r, len(s2)):
-            s2map[s2[i]] = 1 + s2map.get(s2[i], 0)
-            if s2map == s1map:
-                return True
-            s2map[s2[l]] -= 1
-            if s2map[s2[l]] == 0:
-                del s2map[s2[l]]
+        s1_map = [0] * 26
+        s2_map = [0] * 26
+
+        for i in range(len(s1)):
+            s1_map[ord(s1[i])-ord('a')] += 1
+            s2_map[ord(s2[i])-ord('a')] += 1
+        have = 0
+        need = 26
+
+        for i in range(len(s1_map)):
+            if s1_map[i] == s2_map[i]:
+                have += 1
+        if have == need:
+            return True
+        l = 0
+        for r in range(len(s1), len(s2)):
+            c = ord(s2[r])-ord('a')
+            if s1_map[c]== s2_map[c]+1:
+                have += 1
+            elif s1_map[c] == s2_map[c]:
+                have -= 1
+            s2_map[c] += 1
+            c= ord(s2[l]) - ord('a')
+            if s1_map[c] == s2_map[c]-1:
+                have += 1
+            elif s1_map[c] == s2_map[c]:
+                have -= 1
+            s2_map[c] -= 1
             l += 1
-        return False
+            if have == need:
+                return True
+        return have == need
+            
