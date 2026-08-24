033a4fb2f340eeba127770fb23cefdb1dcc4f913
Angus Sun <a86sun@uwaterloo.ca>
2026-08-24 11:58:06 -0700

    [90] [Time Beats: 100.00%] [Memory Beats: 23.86%] - LeetPush

--- 90-Subsets-II.py
+++ 90-Subsets-II.py
@@ -10,7 +10,7 @@ class Solution:
             subset.append(nums[i])
             dfs(i+1)
             subset.pop()
-            while i+1 < len(nums) and i >= 0 and nums[i] == nums[i+1]:
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
                 i += 1
             dfs(i+1)
         dfs(0)
