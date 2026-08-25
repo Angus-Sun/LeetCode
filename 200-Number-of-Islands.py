d0d03a40505c5df647ab358193e590b8d5edcc06
Angus Sun <a86sun@uwaterloo.ca>
2026-08-24 22:23:11 -0700

    [200] [Time Beats: 92.16%] [Memory Beats: 48.03%] - LeetPush

--- 200-Number-of-Islands.py
+++ 200-Number-of-Islands.py
@@ -1,12 +1,11 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
-        visited = [[False for c in range(COLS)] for r in range(ROWS)]
         res = 0
         def dfs(r,c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or visited[r][c] or grid[r][c] == "0":
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
                 return False
-            visited[r][c] = True
+            grid[r][c] = "0"
             dfs(r+1, c)
             dfs(r-1,c)
             dfs(r,c+1)
@@ -14,7 +13,8 @@ class Solution:
             return True
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r,c):
+                if grid[r][c] == "1":
                     res += 1
+                    dfs(r,c)
         return res
             
