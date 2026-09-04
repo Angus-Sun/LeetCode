98692adecb4ccc909bcb517049d473764509183a
Angus Sun <a86sun@uwaterloo.ca>
2026-09-03 23:38:28 -0700

    [417] [Time Beats: 45.58%] [Memory Beats: 39.09%] - LeetPush

--- 417-Pacific-Atlantic-Water-Flow.py
+++ 417-Pacific-Atlantic-Water-Flow.py
@@ -1,26 +1,28 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        res = []
         pac, atl = set(), set()
+        res = []
+
         def dfs(r,c,visited, prev):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or heights[r][c] < prev:
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or heights[r][c] < prev or (r,c) in visited:
                 return
             visited.add((r,c))
-            dfs(r,c+1,visited,heights[r][c])
-            dfs(r,c-1,visited,heights[r][c])
-            dfs(r+1,c,visited,heights[r][c])
-            dfs(r-1,c,visited,heights[r][c])
-
-        for c in range(COLS):
-            dfs(0,c,pac, heights[0][c])
-            dfs(len(heights)-1, c, atl, heights[len(heights)-1][c])
+            dfs(r+1,c, visited, heights[r][c])
+            dfs(r-1,c, visited, heights[r][c])
+            dfs(r,c+1, visited, heights[r][c])
+            dfs(r,c-1, visited, heights[r][c])
+        
         for r in range(ROWS):
             dfs(r,0, pac, heights[r][0])
-            dfs(r,len(heights[0])-1, atl, heights[r][len(heights[0])-1])
+            dfs(r,COLS-1, atl, heights[r][COLS-1])
+        for c in range(COLS):
+            dfs(0,c,pac,heights[0][c])
+            dfs(ROWS-1,c,atl,heights[ROWS-1][c])
         
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) in atl and (r,c) in pac:
+                if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
         return res
+            
